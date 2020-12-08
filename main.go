40995b071d63868ebd661afe4077011fe1b4cfa5
ward5300 <devinw1325wc@gmail.com>
2020-12-08 07:25:47 -0600

    - added gorilla/mux router - added custom 404 handling

--- main.go
+++ main.go
@@ -3,19 +3,31 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
-	} else if r.URL.Path == "/contact" {
-		fmt.Fprint(w, "To get in touch, please send an email to"+
-			" <a href=\"mailto:fakesupport@lenslocked.com\">fakesupport@lenslocked.com</a>.")
-	}
+	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "To get in touch, please send an email to"+
+		" <a href=\"mailto:fakesupport@lenslocked.com\">fakesupport@lenslocked.com</a>.")
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>Oops! Could not find the page you were looking for.</h1>")
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	router := mux.NewRouter()
+	router.NotFoundHandler = http.HandlerFunc(notFound)
+	router.HandleFunc("/", home)
+	router.HandleFunc("/contact", contact)
+	http.ListenAndServe(":3000", router)
 }
